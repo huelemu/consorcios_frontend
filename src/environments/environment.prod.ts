fa21a1bf43cccf524091efe2d97241da6bfe1e34
juanlacy <juan.lacy@huelemu.com.ar>
2025-10-24 19:08:04 -0300

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://apiconsorcios.huelemu.com.ar'
+  apiUrl: 'https://apiconsorcios.huelemu.com.ar',
+  googleClientId: '214922698115-lsq51vuo1k32l9nknl96nu6bccrlfb48.apps.googleusercontent.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:7000'
+  apiUrl: 'http://localhost:7000',
+  googleClientId: '214922698115-lsq51vuo1k32l9nknl96nu6bccrlfb48.apps.googleusercontent.com'
 };
